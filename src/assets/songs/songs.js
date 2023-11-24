c7fe88af2f3198a9b5b960f0dd464679d8ece7c9
SMGhulam29091993 <112707106+SMGhulam29091993@users.noreply.github.com>
2023-11-25 03:21:18 +0530

    Add files via upload

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,15 @@
+import music1 from "./stay.mp3";
+import music2 from "./senorita.mp3";
+import music3 from "./yummy.mp3";
+import music4 from "./tumse-Mohabbat.mp3";
+import music5 from "./Julia-Michaels-Heaven.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+	music4,
+	music5
+};
+
+export default songs;
